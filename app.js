6e4715386415e3ee5ca9b72d182c4258d4fcd8de
Marlene Sedano <marlenesedanodasilva@gmail.com>
2021-03-15 22:40:46 -0400

    Calculo IMC inicio

---
+++ app.js
@@ -0,0 +1,19 @@
+function calculo(peso, altura) {
+  let result;
+  let saida;
+  result = peso / Math.pow(altura, 2);
+  if (result > 18.5 && result < 24.9) {
+    saida = document.getElementById("resultado").innerText =
+      "IMC: " + result.toFixed(2) + ". Você esta dentro do peso Normal.";
+  } else {
+    saida = document.getElementById("resultado").innerText =
+      "IMC: " + result.toFixed(2) + " Você esta fora do peso Normal.";
+  }
+}
+
+document.getElementById("envio").addEventListener("click", () => {
+  const peso = document.getElementById("peso").value;
+  const altura = document.getElementById("altura").value;
+
+  calculo(peso, altura);
+});
